2a30447473cc01f3b3ac9125467280c13e7efc15
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-06-28 16:39:28 +0900

    Update trade_reader.py

--- trade_reader.py
+++ trade_reader.py
@@ -1,3 +1,15 @@
-trades = read_trade_db()
+from notion_client import Client
+import os
 
-print(trades)
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+TRADE_DB_ID = os.environ["NOTION_TRADE_DB_ID"]
+
+notion = Client(auth=NOTION_TOKEN)
+
+
+def read_trade_db():
+    response = notion.databases.query(
+        database_id=TRADE_DB_ID
+    )
+
+    return response["results"]
