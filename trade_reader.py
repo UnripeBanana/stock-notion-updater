d75f91500a0f1b9c33ba19c42bc995c3070f0667
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-06-28 17:29:16 +0900

    Update trade_reader.py

--- trade_reader.py
+++ trade_reader.py
@@ -8,8 +8,42 @@ notion = Client(auth=NOTION_TOKEN)
 
 
 def read_trade_db():
+
     response = notion.databases.query(
         database_id=TRADE_DB_ID
     )
 
-    return response["results"]
+    trades = []
+
+    for page in response["results"]:
+
+        props = page["properties"]
+
+        trade = {
+
+            "page_id": page["id"],
+
+            # 관계형
+            "ticker": (
+                props["종목명"]["select"]["name"]
+                if props["종목명"]["select"] else None
+            ),
+
+            # 선택
+            "type": props["매수/매도"]["select"]["name"],
+
+            # 날짜
+            "date": props["날짜"]["date"]["start"],
+
+            # 숫자
+            "qty": props["수량"]["number"],
+
+            "price": props["단가"]["number"],
+
+            "amount": props["거래금액"]["number"]
+
+        }
+
+        trades.append(trade)
+
+    return trades
